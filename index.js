c34e6e2a970944cd18ee935423c870817d7e8d15
Tari Namikpoh <tarinamikpoh@gmail.com>
2023-06-17 16:22:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var burgerBtn = document.querySelector('.Burger')
+var pizzaBtn = document.querySelector('.Pizza')
+var burgerCard = document.querySelector('.burger-sec')
+var pizzaCard = document.querySelector('.pizza-sec')
+
+for (let i = 0; i < burgerCard.length; i++) {
+    burgerBtn.addEventListener('click', function () {
+        burgerCard[i].style.display ="block"
+        pizzaCard.style.display ="none"
+    
+    })
+    
+}
+// burgerBtn.addEventListener('click', function () {
+   
+// })
+
+pizzaBtn.addEventListener('click', function () {
+    burgerCard.style.display ="none"
+    pizzaCard.style.display ="block"
+
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation:{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
