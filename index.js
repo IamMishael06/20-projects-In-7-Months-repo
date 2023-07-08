3c564e2f195f40c35ed73df3c7e30813904dee5e
Tari Namikpoh <tarinamikpoh@gmail.com>
2023-07-08 10:18:05 +0100

    hamburger added

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-var burgerBtn = document.querySelector('.Burger')
-var pizzaBtn = document.querySelector('.Pizza')
-var burgerCard = document.querySelector('.burger-sec')
-var pizzaCard = document.querySelector('.pizza-sec')
+// var burgerBtn = document.querySelector('.Burger')
+// var pizzaBtn = document.querySelector('.Pizza')
+// var burgerCard = document.querySelector('.burger-sec')
+// var pizzaCard = document.querySelector('.pizza-sec')
 
-for (let i = 0; i < burgerCard.length; i++) {
-    burgerBtn.addEventListener('click', function () {
-        burgerCard[i].style.display ="block"
-        pizzaCard.style.display ="none"
+// for (let i = 0; i < burgerCard.length; i++) {
+//     burgerBtn.addEventListener('click', function () {
+//         burgerCard[i].style.display ="block"
+//         pizzaCard.style.display ="none"
     
-    })
+//     })
     
-}
+// }
 // burgerBtn.addEventListener('click', function () {
    
 // })
 
-pizzaBtn.addEventListener('click', function () {
-    burgerCard.style.display ="none"
-    pizzaCard.style.display ="block"
+// pizzaBtn.addEventListener('click', function () {
+//     burgerCard.style.display ="none"
+//     pizzaCard.style.display ="block"
 
+// })
+var menuBtn = document.querySelector('.bar')
+var dashboard = document.querySelector('.hamburger-nav')
+var bar = document.querySelector('.bar div:nth-child(2)')
+var menu = document.querySelector('.hamburger-nav .menu-items')
+console.log(menuBtn)
+menuBtn.addEventListener('click', ()=>{
+  dashboard.classList.toggle('active')
+  bar.classList.toggle('active')
+  menu.classList.toggle('active')
 })
 
 const swiper = new Swiper('.swiper', {
@@ -42,3 +52,4 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
