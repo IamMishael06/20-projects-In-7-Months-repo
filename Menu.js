0cce8b24450748aa4b3ce6bcb49c0db52efd1f30
Tari Namikpoh <tarinamikpoh@gmail.com>
2023-09-18 22:54:40 +0100

    finished dark mode

--- Menu.js
+++ Menu.js
@@ -10,20 +10,131 @@ arrow.addEventListener('click', ()=>{
 
 // light and dark modes
 
-var darkbtn = document.querySelector('.dark')
-var lightBtn = document.querySelector('.light')
-var slider = document.querySelector('.mode-slider-bg')
-var textChange = document.querySelector('.slider-text h1')
-var iconChange = document.querySelector('.slider-text i')
-var links = document.querySelector('aside li')
+const darkbtn = document.querySelector('.dark')
+const lightBtn = document.querySelector('.light')
+const slider = document.querySelector('.mode-slider-bg')
+const textChange = document.querySelector('.slider-text h1')
+const iconChange = document.querySelector('.slider-text i')
+const links = document.querySelectorAll('aside li')
+const sideNav = document.querySelector('.aside-nav')
+const marsText = document.querySelector('.mars')
+const burgerText = document.querySelector('.burger')
+const quickOrder = document.querySelector('.quick-order')
+const orderText = document.querySelector('.order-head')
+const topMenu = document.querySelector('.top-menu')
+const pizza = document.querySelector('.pizza')
+const coffee = document.querySelector('.coffee')
+const water = document.querySelector('.water')
+const bodyBg = document.querySelector('body')
+const headerText = document.querySelectorAll('.header h1')
+const modalSlider = document.querySelector('.modal-slider')
+const itemDetail = document.querySelectorAll('.item-detail h1')
+const cardBody  = document.querySelectorAll('.card-body')
+const quickOrderCard = document.querySelectorAll('.card-pick-order')
+const history = document.querySelector('.history-text')
+const historyText = document.querySelector('.history-text h1')
+const historyCard = document.querySelectorAll('.history-cards')
+const historyInfo = document.querySelectorAll('.history-info h1')
+darkbtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    slider.style.right = '50%';
+    for (let i = 0; i < links.length; i++) {
+        const changeLink = links[i];
+        changeLink.classList.add('active')
+    }
+    for (let m = 0; m < cardBody.length; m++) {
+        const card = cardBody[m];
+        card.classList.add('active')
+    }
+    for (let t = 0; t < itemDetail.length; t++) {
+        const itemDetails = itemDetail[t];
+        itemDetails.classList.add('active')
+    }
+    for (let n = 0; n < headerText.length; n++) {
+        const headerTexts = headerText[n];
+        headerTexts.classList.add('active')
+    }
+    for (let k = 0; k < quickOrderCard.length; k++) {
+        const quickOrderCards = quickOrderCard[k];
+        quickOrderCards.classList.add('active')
+    }
+    for (let w = 0; w < historyCard.length; w++) {
+        const historyCards = historyCard[w];
+        historyCards.classList.add('active');
+        
+    }
+    for (let e = 0; e < historyInfo.length; e++) {
+        const historyInfos = historyInfo[e];
+        historyInfos.classList.add('active')
+    }
+    section.classList.add('change')
+    sideNav.classList.add('active')
+    marsText.classList.add('active')
+    burgerText.classList.add('active')
+    quickOrder.classList.add('change')
+    orderText.classList.add('active')
+    topMenu.classList.add('active')
+    pizza.classList.add('active')
+    coffee.classList.add('active')
+    water.classList.add('active')
+    bodyBg.classList.add('active')
+    slider.classList.add('active')
+    history.classList.add('change')
+    historyText.classList.add('active')
 
-darkbtn.addEventListener('click', ()=>{
-    slider.style.right = '50%'
-    links.classList.add('active')
 })
-lightBtn.addEventListener('click', ()=>{
+lightBtn.addEventListener('click', (g)=>{
+    g.preventDefault()
     slider.style.right = '0';
-    links.classList.remove('active')
+    for (let i = 0; i < links.length; i++) {
+        const changeLink = links[i];
+        changeLink.classList.remove('active')
+    }
+    for (let m = 0; m < cardBody.length; m++) {
+        const card = cardBody[m];
+        card.classList.remove('active')
+        
+    }
+    for (let t = 0; t < itemDetail.length; t++) {
+        const itemDetails = itemDetail[t];
+        itemDetails.classList.remove('active')
+        
+    }
+    for (let n = 0; n < headerText.length; n++) {
+        const headerTexts = headerText[n];
+        headerTexts.classList.remove('active')
+        
+    }
+    for (let k = 0; k < quickOrderCard.length; k++) {
+        const quickOrderCards = quickOrderCard[k];
+        quickOrderCards.classList.remove('active')
+    }
+    for (let w = 0; w < historyCard.length; w++) {
+        const historyCards = historyCard[w];
+        historyCards.classList.remove('active');
+        
+    }
+    for (let e = 0; e < historyInfo.length; e++) {
+        const historyInfos = historyInfo[e];
+        historyInfos.classList.remove('active')
+    }
+    historyText.classList.remove('active')
+    history.classList.remove('change')
+    section.classList.remove('change')
+    sideNav.classList.remove('active')
+    marsText.classList.remove('active')
+    burgerText.classList.remove('active')
+    quickOrder.classList.remove('change')
+    orderText.classList.remove('active')
+    topMenu.classList.remove('active')
+    pizza.classList.remove('active')
+    coffee.classList.remove('active')
+    water.classList.remove('active')
+    bodyBg.classList.remove('active')
+    headerText.classList.remove('active')
+    slider.classList.remove('active')
+
+
 })
 
 
