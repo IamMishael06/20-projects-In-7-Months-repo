43faa79ef457eb0695eeca75f5fc4c701e653c2c
Tari Namikpoh <tarinamikpoh@gmail.com>
2024-04-02 08:08:15 +0100

    first commit

--- js/map.js
+++ js/map.js
@@ -1,8 +1,5 @@
-// const address = "";
-// const address = "Adetokunbo Ademola Street, Victoria ";
+
 const address = "Tafawa Balewa Square, Lagos Island, Lagos, Nigeria"; 
-// A well-known address for testing
-// const address = document.querySelector('.address').textContent;
 console.log(address)
 // Construct the API URL with the address
 const nominatimGeocodeUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
@@ -96,85 +93,3 @@ fetch(nominatimGeocodeUrl)
     // You can add more markers or perform other actions here
   }
   
-  
-
-// Log the userAddress to check if it's correct
-
-//       // Calculate and display the distance between the two markers
-//       var distance = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, secondAddressLatLng);
-//       console.log('Distance between markers: ' + distance + ' meters');
-//     } else {
-//       console.error('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-// }
-// var map;
-// const addressText = document.getElementById('address')
-// const address = addressText.textContent
-// console.log(address)
-// function initMap() {
-//   // Define initial map options
-//   var mapOptions = {
-//     center: { lat: 6.441290, lng:  3.849760 }, // Coordinates for San Francisco
-//     zoom: 11, // Initial zoom level
-//     disableDefaultUI : false,
-//     maxZoom : 30,
-//     minZoom : 9,
-//   };
-
-//   // Create the map instance
-//    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-// }
-
-// var geocoder = new google.maps.Geocoder();
-//   geocoder.geocode({ 'address': address }, function(results, status) {
-//     if (status === 'OK') {
-//       var userLatLng = results[0].geometry.location;
-      
-      
-//       // Now you can use the userLatLng to display a marker on the map
-//       var userMarker = new google.maps.Marker({
-//         position: userLatLng,
-//         map: map,
-//         title: 'User Address'
-//       });
-      
-//       // You can also add a marker for the second address
-//       var secondAddressLatLng = new google.maps.LatLng(6.441290, 3.849760);
-//       var secondAddressMarker = new google.maps.Marker({
-//         position: secondAddressLatLng,
-//         map: map,
-//         title: 'Second Address'
-//       });
-
-//       // Calculate and display the distance between the two markers
-//       var distance = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, secondAddressLatLng);
-//       console.log('Distance between markers: ' + distance + ' meters');
-//     } else {
-//       console.error('Geocode was not successful for the following reason: ' + status);
-//     }
-//   });
-//(function (window, google) {
-    //     // map options
-    //     var options = {
-    //         center :{
-    //             lat:6.441290,
-    //             lng: 3.849760
-    //         },
-    //         zoom : 11,
-    //         disableDefaultUI : false,
-    //         maxZoom : 15,
-    //         minZoom : 9,
-    //         zoomControlOptions:{
-    //             position : google.maps.ControlPosition.LEFT_BOTTOM,
-    //             style: google.maps.ZoomControlStyle.SMALL
-    
-    //         },
-    //         panControlOptions :{
-    //             position : google.mapS.ControlPosition.RIGHT_BOTTOM
-    //         }
-    //     }
-    //     var mapArea = document.getElementById('map-canvas'),
-    //     // map
-    //     map = new google.maps.Map(mapArea, options)
-    // }(window, google))
